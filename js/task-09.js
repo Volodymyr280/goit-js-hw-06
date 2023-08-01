4988466808ed3ef0bb0c9f4e27c455c348eec763
Volodymyr280 <128935680+Volodymyr280@users.noreply.github.com>
2023-08-01 21:25:11 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorValue = document.querySelector('.color');
+
+btn.addEventListener('click', onClick);
+
+function onClick(evt) {
+  let randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColorValue.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
