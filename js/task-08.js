48e31511f60f3884d9be1c3c60a1f8230ec8f586
Volodymyr280 <128935680+Volodymyr280@users.noreply.github.com>
2023-08-01 21:12:22 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', handlerSubmit);
 
+function handlerSubmit(evt) {
+    evt.preventDefault();
+    const { email, password } = evt.currentTarget.elements;
+    
+    if (email.value === '' || password.value === '') {
+        alert('Please fill in all fields');
+    }
+    console.log({ Email: email.value, Password: password.value });
+    evt.currentTarget.reset();
+}
